96854e675da5dc66925d12413a35eefe67858d89
ValiantGinger <david.silady@gmail.com>
2018-11-28 21:42:41 +0100

    reordered the functions.h

--- functions.h
+++ functions.h
@@ -1,18 +1,32 @@
 #ifndef FUNCTIONS_H_INCLUDED
 #define FUNCTIONS_H_INCLUDED
 
-int countEntries(FILE *f_p);
-void fread_lines(FILE *f_p, char line[LINE_NUM][MAX_LINE_LENGTH]);
-void read_lines(char line[LINE_NUM][MAX_LINE_LENGTH]);
-void fill_node(struct car_list *car_current,char line[LINE_NUM][MAX_LINE_LENGTH]);
-void free_node(struct car_list **car_current);
+//Working w/ strings
 char *s_toupper(char const *line);
 int found_substring(char *haystack, char *needle);
-void read_line(char *line);
+
+//Adding/Filling
 void add_node(struct car_list **car_current, struct car_list **car_new);
+void fill_node(struct car_list *car_current,char line[LINE_NUM][MAX_LINE_LENGTH]);
+
+//Reading
+	//Read multiple lines
+void fread_lines(FILE *f_p, char line[LINE_NUM][MAX_LINE_LENGTH]);
+void read_lines(char line[LINE_NUM][MAX_LINE_LENGTH]);
+
+	//Reads 1 line
+void read_line(char *line);
+int countEntries(FILE *f_p);
+
+//Deleting
 void delete_nodes(struct car_list **car_first);
+void free_node(struct car_list **car_current);
+
+//Printing
 void print_all(struct car_list **car_first);
 void print_node(struct car_list *car_current);
+
+//Alloc
 void alloc_first(struct car_list **car_current);
 void alloc_next(struct car_list **car_current);
 
