3494f68c645ef9cc85e0b4e769b869ddfbbfefa9
ValiantGinger <david.silady@gmail.com>
2018-12-10 23:24:22 +0100

    c

--- main_functions.c
+++ main_functions.c
@@ -89,15 +89,6 @@ void find(struct car_list **car_first) {
 	}
 }
 
-int find_match(char wanted_manufacturer[MAX_LINE_LENGTH], 
-				int max_price,
-				struct car_list *car_current) {
-	if (strcmp(s_toupper(wanted_manufacturer), s_toupper(car_current->manufacturer)) && car_current->price <= max_price) {
-		return 0;
-	}
-	else return 1;
-}
-
 
 void update(struct car_list **car_first) {
 	struct car_list *car_current = *car_first;
